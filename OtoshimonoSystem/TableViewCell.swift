5fd94aea0671b050f6d41f8923174a74dd8afb02
Azuma Sato <lifeghtcloerce5291@gmail.com>
2017-10-06 17:29:52 +0900

    storyboard, Realm, SwiftyJSON

---
+++ OtoshimonoSystem/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  OtoshimonoSystem
+//
+//  Created by Azuma on 2017/10/06.
+//  Copyright © 2017年 Azuma. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
